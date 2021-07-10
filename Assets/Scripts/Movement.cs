74a8fd9539d4da3bf65ef135be634125697cf097
heimu177 <56528863+heimu177@users.noreply.github.com>
2021-07-11 06:18:41 +0900

    added audio

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rbd;
+    AudioSource audioSource;
+    [SerializeField] float mainThrust = 800f;
+    [SerializeField] float rotationThrust = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbd = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (!audioSource.isPlaying)
+                audioSource.Play();
+            rbd.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        // freezing rotation so we can manually rotate
+        rbd.freezeRotation = true;
+
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+
+        // unfreezing rotation so the physics system wil ltake over
+        rbd.freezeRotation = false;
+    }
+}
